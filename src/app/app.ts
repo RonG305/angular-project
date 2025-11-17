96ba917f2630e9ab4876c52d869da6b10a294329
Ronald Mutia <mutiaronald138@gmail.com>
2025-11-17 14:07:31 +0300

    angular-learn(core): simple counter app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Counter } from './components/counter/counter';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Counter],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
